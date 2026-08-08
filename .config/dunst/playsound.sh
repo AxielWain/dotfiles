be0a23dc94f15243c7abfaeb7fb462fc7370e93b
Axiel Wain <243482860+AxielWain@users.noreply.github.com>
2026-08-08 08:02:58 -0500

    Update dunst config
    - Adjust colors
    - Add script to play sounds for notifications

--- .config/dunst/playsound.sh
+++ .config/dunst/playsound.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
 
-paplay $HOME/.sounds/notify-alert.mp3
+SOUNDS_PATH="$HOME/.sounds"
+DEFAULT_SFX="$SOUNDS_PATH/default_notification.mp3"
+CHAT_SFX="$SOUNDS_PATH/chat_notification.mp3"
+CRITICAL_SFX="$SOUNDS_PATH/critical_notification.mp3"
+NORMAL_SFX="$SOUNDS_PATH/warning_notification.mp3"
+
+if [[ $DUNST_DESKTOP_ENTRY == "discord" ]] then
+  exit 0;
+fi
+
+case "$DUNST_URGENCY" in
+  "CRITICAL")
+    paplay $CRITICAL_SFX
+    ;;
+  "NORMAL")
+    paplay $NORMAL_SFX
+    ;;
+  *)
+    paplay $DEFAULT_SFX
+    ;;
+esac
 
